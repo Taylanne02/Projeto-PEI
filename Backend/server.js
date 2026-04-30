bafc4aa1a0996050ffa47877038fd2e975f48eac
Taylanne <taylannecastelobrancocavalcant@gmail.com>
2026-04-30 19:41:10 -0300

    Subindo arquivos bases

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+// importação das bibliotecas
+const express = require("express");
+
+// iniciação do servidor 
+const app = express();
+const PORT = 3000;
+
+// middlewares
+app.use(express.json());
+
+// verificação de teste para ver se está ativo
+app.get('/', (req, res) => {
+  res.json({ mensagem: 'Servidor funcionando!' });
+});
+
+// iniciação do servidor na porta 3000
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
+});
