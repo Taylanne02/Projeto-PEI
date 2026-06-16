09f42cfd3d9e37fd4be590026259284cf709cfdc
pedrotx03 <pedrotx@rede.ulbra.br>
2026-06-16 15:30:09 -0300

    organizando a pasta professor

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { ProfessorLayout } from '@/components/professor-layout'
-import { DashboardPage } from '@/pages/dashboard-page'
-import { FinancePage } from '@/pages/finance-page'
-import { LessonsPage } from '@/pages/lessons-page'
-import { NewLessonPage } from '@/pages/new-lesson-page'
+import { DashboardPage } from '@/pages/Professor/dashboard-page'
+import { FinancePage } from '@/pages/Professor/finance-page'
+import { LessonsPage } from '@/pages/Professor/lessons-page'
+import { NewLessonPage } from '@/pages/Professor/new-lesson-page'
 import { NotFoundPage } from '@/pages/not-found-page'
 import { StartPage } from '@/pages/start-page'
-import { ReviewsPage } from '@/pages/reviews-page'
+import { ReviewsPage } from '@/pages/Professor/reviews-page'
 
 export function AppRoutes() {
   return (
